48226efb9f54823e30157d112d57ea6156f300ed
noveriojoee <noverio.joe.hasibuan@outlook.co.id>
2021-07-12 23:13:11 +0700

    update toggle button for playing and pause

--- BCGMusicPLayyer/Views/MainViewController.m
+++ BCGMusicPLayyer/Views/MainViewController.m
@@ -14,6 +14,15 @@
 @property (weak, nonatomic) IBOutlet CustomTextField *tfSearchField;
 @property (weak, nonatomic) IBOutlet UITableView *tblView;
 @property MusicCardTableViewCell* selectedCell;
+@property (weak, nonatomic) IBOutlet UIView *playingView;
+@property (weak, nonatomic) IBOutlet UIButton *btnPlay;
+@property (weak, nonatomic) IBOutlet UISlider *sliders;
+@property (weak, nonatomic) IBOutlet UIButton *btnPause;
+
+
+@property CGRect hiddenPlayingFrame;
+@property CGRect showPlayingFrame;
+
 
 @end
 
@@ -32,16 +41,45 @@
     [self.tblView setDelegate:self];
     [self.tblView setDataSource:self];
     [self.tblView registerNib:[UINib nibWithNibName:@"MusicCardTableViewCell" bundle:[NSBundle bundleWithIdentifier:@"com.gid.BCGMusicPLayyer"]] forCellReuseIdentifier:@"song_item_template"];
+    
+    
 }
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
+    self.hiddenPlayingFrame= CGRectMake(0, (self.view.frame.size.height), self.playingView.frame.size.width, (self.playingView.frame.size.height));
+    self.showPlayingFrame = CGRectMake(0, (self.view.frame.size.height - self.playingView.frame.size.height), self.playingView.frame.size.width, self.playingView.frame.size.height);
+    
+    
     [self.viewModel searchMusicWithCompletion:^(NSString *response) {
+        [self hideMusicView:nil];
         if([response isEqualToString:@"OK"]){
             [self.tblView reloadData];
         }
     }];
 }
+
+- (IBAction)btnFF:(id)sender {
+}
+
+- (IBAction)btnBF:(id)sender {
+}
+
+- (IBAction)btnPlay:(id)sender {
+    [self.viewModel.songPlayer play];
+    [self.btnPlay setHidden:YES];
+    [self.btnPause setHidden:NO];
+    
+}
+- (IBAction)btnPause:(id)sender {
+    [self.viewModel.songPlayer pause];
+    [self.selectedCell setTrackIsPause];
+    [self.btnPause setHidden:YES];
+    [self.btnPlay setHidden:NO];
+    [self hideMusicView:sender];
+}
+
+
 
 #pragma tableview delegates
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
@@ -54,8 +92,6 @@
     }else{
         return nil;
     }
-    
-    
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -73,8 +109,11 @@
         if (self.selectedCell != nil){
             [self.selectedCell setTrackIsStop];
         }
-        [cell setTrackIsPlay];
         self.selectedCell = cell;
+        [cell setTrackIsPlay];
+        [self.btnPlay setHidden:YES];
+        [self.btnPause setHidden:NO];
+        [self showMusicView:cell];
     }
 }
 #pragma end
@@ -103,6 +142,35 @@
 
 - (void)audioPlayerDecodeErrorDidOccur:(AVAudioPlayer *)player error:(NSError *)error{
     NSLog(@"Error occured");
+}
+#pragma end
+
+
+#pragma MusicCommandUIView
+-(void)showMusicView : (id)sender{
+    [UIView animateWithDuration:0.25
+                          delay:0.1
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+        self.playingView.frame = self.showPlayingFrame;
+        
+        
+    }
+                     completion:^(BOOL finished){
+        
+    }];
+}
+-(void)hideMusicView : (id)sender{
+    [UIView animateWithDuration:0.25
+                          delay:0.1
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+        self.playingView.frame = self.hiddenPlayingFrame;
+        
+    }
+                     completion:^(BOOL finished){
+       
+    }];
 }
 #pragma end
 
