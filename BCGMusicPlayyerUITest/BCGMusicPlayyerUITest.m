8cfa7bc32040fe0ded74a34b3f7552d25ca4e20a
noveriojoee <noverio.joe.hasibuan@outlook.co.id>
2021-07-18 19:14:14 +0700

    last commit, adding new method on UITest script

--- BCGMusicPlayyerUITest/BCGMusicPlayyerUITest.m
+++ BCGMusicPlayyerUITest/BCGMusicPlayyerUITest.m
@@ -27,44 +27,62 @@
 }
 
 
-- (void)testExample {
-    // UI tests must launch the application that they test.
+-(void)testButtons{
     XCUIApplication *app = [[XCUIApplication alloc] init];
     [app launch];
-
-    XCUIElement* tfSearch = app/*@START_MENU_TOKEN@*/.textFields[@"tf_search"]/*[[".textFields[@\"Search\"]",".textFields[@\"tf_search\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/;
+    
     XCUIElement* btnPlay = app.buttons[@"btn_play"];
     XCUIElement* btnPause = app.buttons[@"btn_pause"];
     XCUIElement* btnFF = app.buttons[@"btn_ff"];
     XCUIElement* btnBF = app.buttons[@"btn_bf"];
-    
-    [tfSearch tap];
-    [tfSearch typeText:@"Adele Hello"];
-    [app.toolbars[@"Toolbar"].buttons[@"Done"] tap];
-    sleep(10);
+    sleep(5);
     [btnPlay tap];
-    sleep(50);
+    //Wait for download songs and play
+    sleep(15);
     [btnPause tap];
     sleep(5);
     [btnPlay tap];
     sleep(5);
-    [btnPause tap];
-    sleep(5);
-    [btnPlay tap];
-    sleep(10);
     [btnFF tap];
-    sleep(25);
+    //Wait for download songs and play
+    sleep(15);
     [btnBF tap];
-    sleep(25);
+    //Wait for download songs and play
+    sleep(15);
     
-    XCUIElementQuery *tablesQuery = [[XCUIApplication alloc] init].tables;
-    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"Uncharted (Deluxe Edition)"]/*[[".cells.staticTexts[@\"Uncharted (Deluxe Edition)\"]",".staticTexts[@\"Uncharted (Deluxe Edition)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ swipeUp];
-    [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:4].staticTexts[@"Hello / Lacrimosa"] swipeDown];
+    XCUIElement *element = [[[[[[XCUIApplication alloc] init].tables childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther] elementBoundByIndex:0];
+    [element swipeUp];
+    [element swipeDown];
+        
+}
+
+- (void)testPlayAndSwipe {
+    // UI tests must launch the application that they test.
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    XCUIElementQuery *tablesQuery = app.tables;
+    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"Trunk Music: Harry Bosch Series, Book 5 (Unabridged)"]/*[[".cells.staticTexts[@\"Trunk Music: Harry Bosch Series, Book 5 (Unabridged)\"]",".staticTexts[@\"Trunk Music: Harry Bosch Series, Book 5 (Unabridged)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ swipeUp];
+    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"Hello"]/*[[".cells.staticTexts[@\"Hello\"]",".staticTexts[@\"Hello\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
     
-    
-    
-    
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCUIElement *btnBfButton = app/*@START_MENU_TOKEN@*/.buttons[@"btn_bf"]/*[[".buttons[@\"backward.end\"]",".buttons[@\"btn_bf\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/;
+    [btnBfButton tap];
+    sleep(5);
+    [btnBfButton tap];
+    sleep(5);
+    [app/*@START_MENU_TOKEN@*/.textFields[@"tf_search"]/*[[".textFields[@\"Search\"]",".textFields[@\"tf_search\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    sleep(5);
+    [app/*@START_MENU_TOKEN@*/.textFields[@"tf_search"]/*[[".textFields[@\"Search\"]",".textFields[@\"tf_search\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ typeText:@"ariel noah"];
+    sleep(5);
+    [app.toolbars[@"Toolbar"].buttons[@"Done"] tap];
+    sleep(5);
+    [/*@START_MENU_TOKEN@*/[tablesQuery.cells containingType:XCUIElementTypeStaticText identifier:@"One for All"].element/*[["tablesQuery","[",".cells containingType:XCUIElementTypeStaticText identifier:@\"ZOMBIES 2 (Original TV Movie Soundtrack)\"].element",".cells containingType:XCUIElementTypeStaticText identifier:@\"Milo Manheim, Meg Donnelly, Kylee Russell, Trevor Tordjman, Carla Jeffery, Chandler Kinney, Pearce Joza, Baby Ariel, Emilia McCarthy, Jasmine Renee Thomas & Noah Zulfikar\"].element",".cells containingType:XCUIElementTypeStaticText identifier:@\"One for All\"].element"],[[[-1,0,1]],[[1,4],[1,3],[1,2]]],[0,0]]@END_MENU_TOKEN@*/ tap];
+    sleep(5);
+    [[tablesQuery.cells containingType:XCUIElementTypeStaticText identifier:@"Bintang Di Surga"].staticTexts[@"peterpan"] tap];
+    sleep(5);
+    [app/*@START_MENU_TOKEN@*/.buttons[@"btn_pause"]/*[[".buttons[@\"pause\"]",".buttons[@\"btn_pause\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    sleep(10);
+   
 }
 
 - (void)testLaunchPerformance {
